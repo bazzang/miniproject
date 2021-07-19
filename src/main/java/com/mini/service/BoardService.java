ce7f37dc87460f734499aed28639742f242da227
jisoo <wltnn9973@gmail.com>
2021-07-19 23:57:46 +0900

    첫번째 커밋

---
+++ src/main/java/com/mini/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.mini.service;
+
+import java.util.List;
+
+import com.mini.domain.BoardDTO;
+
+
+public interface BoardService {
+	public boolean registerBoard(BoardDTO params);
+
+	public BoardDTO getBoardDetail(Long idx);
+
+	public boolean deleteBoard(Long idx);
+
+	public List<BoardDTO> getBoardList();
+
+}
